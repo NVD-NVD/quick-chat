cac111600be3773e540cf6c125487db39566fe53
Dat Nguyen <datnguyen2847@gmail.com>
2024-04-14 20:46:35 +0700

    Setup jest

---
+++ quick-chat-ui/projects/quick-chat-ui/jest.config.js
@@ -0,0 +1,10 @@
+globalThis.ngJest = {
+  skipNgcc: true,
+  tsconfig: 'tsconfig.spec.json',
+};
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+};
